22e45e74ada049f5463dc4f2f3476922821564d1
Akshay Patel <paksha@google.com>
2021-03-01 19:55:39 +0000

    MNIST example

---
+++ mnist.py
@@ -0,0 +1,130 @@
+import time
+import jax.numpy as jnp
+from jax import grad, jit, vmap, random
+from jax.scipy.special import logsumexp
+import numpy as np
+from torch.utils import data
+from torchvision.datasets import MNIST
+
+'''
+This program was created by using the following as a guide:
+
+https://github.com/google/jax/blob/master/docs/notebooks/Neural_Network_and_Data_Loading.ipynb
+'''
+
+# A helper function to randomly initialize weights and biases
+# for a dense neural network layer
+def random_layer_params(m, n, key, scale=1e-2):
+  w_key, b_key = random.split(key)
+  return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
+
+# Initialize all layers for a fully-connected neural network with sizes "sizes"
+def init_network_params(sizes, key):
+  keys = random.split(key, len(sizes))
+  return [random_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+
+layer_sizes = [784, 512, 512, 10]
+param_scale, step_size = 0.1, 0.01
+n_targets, num_epochs, batch_size = 10, 8, 128
+params = init_network_params(layer_sizes, random.PRNGKey(0))
+
+# RELU: 0 if x <= 0 otherwise x
+def relu(x):
+  return jnp.maximum(0, x)
+
+def predict(params, img):
+  activations = img
+  for w, b in params[:-1]:
+    outputs = jnp.dot(w, activations) + b
+    activations = relu(outputs)
+  
+  final_w, final_b = params[-1]
+  logits = jnp.dot(final_w, activations) + final_b
+  return logits - logsumexp(logits)
+
+  # Check that it works on single examples
+random_flattened_image = random.normal(random.PRNGKey(1), (28 * 28,))
+preds = predict(params, random_flattened_image)
+
+random_flattened_images = random.normal(random.PRNGKey(1), (10, 28 * 28))
+
+# Upgrade it to handle batches using `vmap`
+
+# Batched version of the `predict` function
+batched_predict = vmap(predict, in_axes=(None, 0))
+
+# `batched_predict` has the same call signature as `predict`
+batched_preds = batched_predict(params, random_flattened_images)
+
+def one_hot(x, k, dtype=jnp.float32):
+  # Create a one-hot encoding of x of size k.
+  return jnp.array(x[:, None] == jnp.arange(k), dtype)
+  
+def accuracy(params, images, targets):
+  target_class = jnp.argmax(targets, axis=1)
+  predicted_class = jnp.argmax(batched_predict(params, images), axis=1)
+  return jnp.mean(predicted_class == target_class)
+
+def loss(params, images, targets):
+  preds = batched_predict(params, images)
+  return -jnp.mean(preds * targets)
+
+@jit
+def update(params, x, y):
+  grads = grad(loss)(params, x, y)
+  return [(w - step_size * dw, b - step_size * db)
+          for (w, b), (dw, db) in zip(params, grads)]
+
+def numpy_collate(batch):
+  if isinstance(batch[0], np.ndarray):
+    return np.stack(batch)
+  elif isinstance(batch[0], (tuple,list)):
+    transposed = zip(*batch)
+    return [numpy_collate(samples) for samples in transposed]
+  else:
+    return np.array(batch)
+
+class NumpyLoader(data.DataLoader):
+  def __init__(self, dataset, batch_size=1,
+                shuffle=False, sampler=None,
+                batch_sampler=None, num_workers=0,
+                pin_memory=False, drop_last=False,
+                timeout=0, worker_init_fn=None):
+    super(self.__class__, self).__init__(dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        sampler=sampler,
+        batch_sampler=batch_sampler,
+        num_workers=num_workers,
+        collate_fn=numpy_collate,
+        pin_memory=pin_memory,
+        drop_last=drop_last,
+        timeout=timeout,
+        worker_init_fn=worker_init_fn)
+
+class FlattenAndCast(object):
+  def __call__(self, pic):
+    return np.ravel(np.array(pic, dtype=jnp.float32))
+
+mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=FlattenAndCast())
+training_generator = NumpyLoader(mnist_dataset, batch_size=batch_size, num_workers=0)
+
+# Get the full train and test datasets
+train_images = np.array(mnist_dataset.train_data).reshape(len(mnist_dataset.train_data), -1)
+train_labels = one_hot(np.array(mnist_dataset.train_labels), n_targets)
+mnist_dataset_test = MNIST('/tmp/mnist/', download=True, train=False)
+test_images = jnp.array(mnist_dataset_test.test_data.numpy().reshape(len(mnist_dataset_test.test_data), -1), dtype=jnp.float32)
+test_labels = one_hot(np.array(mnist_dataset_test.test_labels), n_targets)
+
+for epoch in range(num_epochs):
+  start = time.time()
+  for x, y in training_generator:
+    y = one_hot(y, n_targets)
+    params = update(params, x, y)
+  epoch_time = time.time() - start
+
+  train_acc = accuracy(params, train_images, train_labels)
+  test_acc = accuracy(params, test_images, test_labels)
+  print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+  print("Training accuracy {}".format(train_acc))
+  print("Test accuracy {}".format(test_acc))
