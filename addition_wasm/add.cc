f0f7dc0f5a61ed86e60f32320cc7e32ef3f68290
Akshay Patel <paksha@google.com>
2021-04-06 13:55:36 +0000

    argument passing

--- addition_wasm/add.cc
+++ addition_wasm/add.cc
@@ -1,22 +1,17 @@
 #include <iostream>
 #include "add.h"
 
-int main(int argc, char** argv) {
-  Add addition;
+extern "C" {
+  int addNums(int a, int b) {
+    Add addition;
 
-  // Set up args and run the computation.
-  const float args[2] = {1, 3};
-  std::copy(args + 0, args + 1, addition.arg0_data());
-  std::copy(args + 1, args + 2, addition.arg1_data());
-  addition.Run();
+    // Set up args and run the computation.
+    const float args[2] = {(float)a, (float)b};
+    std::copy(args + 0, args + 1, addition.arg0_data());
+    std::copy(args + 1, args + 2, addition.arg1_data());
+    addition.Run();
 
-  // Check result
-  if (addition.result0() == 4) {
-    std::cout << "Success! Found expected value of 4." << std::endl;
-  } else {
-    std::cout << "Failed. Expected value 4 at 0,0. Got:"
-              << addition.result0() << std::endl;
+    return addition.result0();
   }
 
-  return 0;
 }
